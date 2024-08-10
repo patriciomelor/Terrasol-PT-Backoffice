44a4b5a092ad04f2286780818919c77930ec24ae
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2024-08-09 20:30:00 -0400

    reinit

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4101b09439480bab8fb557440250e712::getLoader();
+return ComposerAutoloaderInit7e8c3c14ff33b199b4a0838993eb8423::getLoader();
