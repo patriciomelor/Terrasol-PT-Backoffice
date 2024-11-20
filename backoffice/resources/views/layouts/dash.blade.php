15455090fa924953431f458e43763824f6014d51
Patricio Melo <pmelo@geovictoria.com>
2024-11-20 13:54:23 -0300

    FIX FOOTER DASH

--- backoffice/resources/views/layouts/dash.blade.php
+++ backoffice/resources/views/layouts/dash.blade.php
@@ -330,16 +330,7 @@
                                     <a href="https://www.terrasol.cl" target="_blank"
                                         class="footer-link">TERRASOL</a>
                                 </div>
-                                <div class="d-none d-lg-inline-block">
-                                    <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
-                                        target="_blank">License</a>
-                                    <a href="https://1.envato.market/pixinvent_portfolio" target="_blank"
-                                        class="footer-link me-4">More Themes</a>
-                                    <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/"
-                                        target="_blank" class="footer-link me-4">Documentation</a>
-                                    <a href="https://pixinvent.ticksy.com/" target="_blank"
-                                        class="footer-link d-none d-sm-inline-block">Support</a>
-                                </div>
+
                             </div>
                         </div>
                     </footer>
